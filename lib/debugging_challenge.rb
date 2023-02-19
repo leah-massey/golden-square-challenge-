09f40fb99182503aecbe1bb932a7751c940e3d66
Leah Massey <leahmassey@MASTUDE077WR31G.cust.communityfibre.co.uk>
2023-02-19 14:36:34 +0000

    debugging_challenge

--- lib/debugging_challenge.rb
+++ lib/debugging_challenge.rb
@@ -1,12 +1,24 @@
 def get_most_common_letter(text)
     counter = Hash.new(0)
+    
     text.chars.each do |char|
-      counter[char] += 1
+ 
+        if char =~ /[A-Za-z]/
+        counter[char] += 1 
+
+        end
     end
-    counter.to_a.sort_by { |k, v| v }[0][0]
-  end
+ 
+    counter.to_a.sort_by { |k, v| v }.reverse![0][0]
+end
   
+  puts get_most_common_letter("the roof, the roof, the roof is on fire!")
+
   # Intended output:
-  # 
+  
+
+  # > get_most_common_letter("roof")
+  # => "o"
+
   # > get_most_common_letter("the roof, the roof, the roof is on fire!")
   # => "o"
